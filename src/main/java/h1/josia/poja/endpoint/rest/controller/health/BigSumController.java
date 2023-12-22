1e4f5910d6e77c5fa83843703ec6f4a1b927b684
josiaJj <hei.josia@gmail.com>
2023-12-22 09:10:44 +0300

    feat: add BigSumController

---
+++ src/main/java/h1/josia/poja/endpoint/rest/controller/health/BigSumController.java
@@ -0,0 +1,19 @@
+package h1.josia.poja.endpoint.rest.controller.health;
+
+import java.math.BigInteger;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BigSumController {
+    private final BigSumService bigSumService;
+
+    @GetMapping("/big-sum")
+    public BigInteger add(@RequestParam BigInteger a, @RequestParam BigInteger b) {
+        return a.add(b);
+    }
+}
+
