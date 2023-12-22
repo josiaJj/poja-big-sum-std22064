c98b261c09bf033ba8ea77126b49d7f119fdb729
josiaJj <hei.josia@gmail.com>
2023-12-22 09:18:36 +0300

    test: add BigSumController to rest.controller

---
+++ src/test/java/h1/josia/poja/endpoint/rest/controller/BigSumControllerIT.java
@@ -0,0 +1,25 @@
+package h1.josia.poja.endpoint.rest.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import h1.josia.poja.endpoint.rest.controller.health.BigSumController;
+import h1.josia.poja.conf.FacadeIT;
+import java.math.BigInteger;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BigSumControllerIT extends FacadeIT {
+    @Autowired
+    BigSumController bigSumController;
+
+    @Test
+    void can_do_addition() {
+        BigInteger a = BigInteger.valueOf(5);
+        BigInteger b =BigInteger.valueOf(2);
+
+        BigInteger actual = bigSumController.add(a, b);
+
+        assertEquals(BigInteger.valueOf(7), actual);
+    }
+}
+
